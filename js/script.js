2112fb4cdb3a527bbe5143a3e6db3d3fbcc15014
MatteoDiMartino <matteodimartino359@gmail.com>
2024-03-06 14:02:59 +0100

    feat: add input base html and js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Chiedo nome
+
+    let userName = document.querySelector('#inNome');
+    console.log (userName)
+// Chiedo cognome
+
+    let userSurname = document.querySelector('#inCognome');
+    console.log (userSurname)
+// Chiedo Km
+
+    let userKm = document.querySelector('#inKm');
+    console.log (userKm)
+// Chiedo età
+
+    let userAge = document.querySelector('#inAge');
+    console.log (userAge)
+// Definisco costo
+
+// Definisco sconti
+
+// Calcolo finale
