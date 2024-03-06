25c4bdbdb832ef96586592625671930a95a1d318
MatteoDiMartino <matteodimartino359@gmail.com>
2024-03-06 17:30:27 +0100

    feat: add calc ticket without discount

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 // Chiedo età
-
 // Chiedo km
-
 // Definisco costo
-
 // Definisco sconti
-
 // Calcolo finale
-
+// Seleziono bottone
 // genera risultato
+
+const buttonResult = document.querySelector('#btn-result');
+console.log (buttonResult)
+buttonResult.addEventListener('click', function () {
+    const userage = parseInt(document.querySelector('#inAge').value);
+    console.log (userage);
+
+    const kmUser = parseInt(document.querySelector('#inKm').value);
+    console.log (kmUser);
+
+    const costKm = 0.21;
+
+    const totalTicket = (kmUser * costKm);
+    console.log (totalTicket);
+});
